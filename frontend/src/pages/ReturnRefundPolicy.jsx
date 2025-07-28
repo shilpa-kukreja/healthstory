8bfc9360f8dfd1d05983419ebf5554cad5ce11fe
Shubham Shukla <shubshukla2332@gmail.com>
2025-07-28 17:11:19 +0530

    some changes

--- frontend/src/pages/ReturnRefundPolicy.jsx
+++ frontend/src/pages/ReturnRefundPolicy.jsx
@@ -26,7 +26,7 @@ const ReturnRefundPolicy = () => {
                             <h4>  GLOBAL RETURN AND REFUND POLICY FOR HEALTHSTORY </h4>
                         </div>
                         <div className="common_para">
-                            <p>   This Privacy Policy describes how Albelee (the \"Site,\" \"we,\" \"us,\" or \"our\") collects, uses, and discloses your personal information when you visit, use our services, or make a purchase from https://albelee.in (the \"Site\") or otherwise communicate with us (collectively, the \"Services\"). For purposes of this Privacy Policy, \"you\" and \"your\" mean you as the user of the Services, whether you are a customer, website visitor, or another individual whose information we have collected pursuant to this Privacy Policy.</p>
+                            <p>   This Privacy Policy describes how healthstory(the \"Site,\" \"we,\" \"us,\" or \"our\") collects, uses, and discloses your personal information when you visit, use our services, or make a purchase from https://healthstory.in (the \"Site\") or otherwise communicate with us (collectively, the \"Services\"). For purposes of this Privacy Policy, \"you\" and \"your\" mean you as the user of the Services, whether you are a customer, website visitor, or another individual whose information we have collected pursuant to this Privacy Policy.</p>
 
                             <p>
                                 Please read this privacy policy carefully. By using and accessing any of the services, you agree to the collection, use, and disclosure of your information as described in this privacy policy. If you do not agree to this privacy policy, please do not use or access any of the services.
