c78a95397c60cff59e1459ca9dcf93f333bfdd82
Shubham Shukla <shubshukla2332@gmail.com>
2025-08-22 17:02:58 +0530

    some changes

--- frontend/src/components/LogIn.jsx
+++ frontend/src/components/LogIn.jsx
@@ -26,7 +26,7 @@ const OTPLogin = () => {
       const response = await fetch("https://healthstory.net.in/api/auth/send-otp", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ phone }),
+        body: JSON.stringify({ number : phone }),
       });
 
       const data = await response.json();
